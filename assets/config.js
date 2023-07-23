0c7bbb23011955ed77b33958ad670c432ad13f5a
JustSomeNoname <noname.discord@protonmail.com>
2023-07-23 15:07:50 +0200

    commit #1 -> Move from .io/noname/

---
+++ assets/config.js
@@ -0,0 +1,58 @@
+const config = {
+
+    USERNAME: "Brian",
+    PICTURE: "assets/img/profile_picture.png",
+
+    DESCRIPTION: '<i>he/him</i><br><br> /\\___/\\<br>꒰ ˶• ༝ - ˶꒱<br>./づ~🍨',
+
+    PROJECTS: [
+        {
+            id: "diablo4",
+            image: "assets/img/diablo4.png",
+            link: "https://discord.com/invite/diablo4",
+            name: "Sanctuary - Diablo 4 Community",
+            description: "Coming Soon"
+        },
+        {
+            id: "amar",
+            image: "assets/img/amar.gif",
+            link: "https://discord.com/servers/amar-444141193022930944",
+            name: "Amar",
+            description: "Coming Soon"
+        },
+        {
+            id: "modhq",
+            image: "assets/img/modhq.gif",
+            link: "https://discord.com/invite/modhq",
+            name: "Moderatoren HQ",
+            description: "Coming Soon"
+        },
+        {
+            id: "dreamycafe",
+            image: "assets/img/dreamycafe.gif",
+            link: "https://discord.com/invite/dreamycafe",
+            name: "Dreamy Café | Anime ・ Gaming",
+            description: "Coming Soon"
+        },
+        {
+            id: "agc",
+            image: "assets/img/animegamingcafe.gif",
+            link: "https://animegamingcafe.de",
+            name: "Anime & Gaming Cafe",
+            description: "Coming Soon"
+        },
+        {
+            id: "teamruby",
+            image: "assets/img/rubychan.png",
+            link: "https://discord.com/invite/rubychan",
+            name: "#TeamRuby",
+            description: "Coming Soon"
+        },
+    ],
+
+    SOCIALS: [{
+        class_name: "fab fa-github",
+        link: "https://github.com/JustSomeNoname"
+    }, ],
+
+}
